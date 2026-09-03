47915bfac241a87834edac6911df7612f0af73c8
Mahmoud-Hussien-Mohamed <mahmoudhussienmohamed.mhm@gmail.com>
2026-09-03 22:59:07 +0300

    Adding player and basic main window

---
+++ Player.py
@@ -0,0 +1,107 @@
+from enum import Enum
+import os
+import turtle
+
+IMGS_DIR = os.path.abspath('./assets')
+AIRCRAFTS_DIR = os.path.join(IMGS_DIR, 'aircrafts')
+PLAYER_DIR = os.path.join(AIRCRAFTS_DIR, 'player')
+
+LEFT_PATH = os.path.join(PLAYER_DIR, 'left1.gif')
+RIGHT_PATH = os.path.join(PLAYER_DIR, 'right1.gif')
+NORMAL_PATH = os.path.join(PLAYER_DIR, 'normal.gif')
+
+SPEED = 5
+
+class Player:
+
+    class Movement(Enum):
+        LEFT    = -1
+        RIGHT   =  1
+        UP      =  0
+        DOWN    =  0
+
+    def __init__(self, imgs_dir: str = PLAYER_DIR, speed: float = SPEED):
+        self.init_img_paths(imgs_dir)
+        self.init_lookups()
+        self.speed = speed
+        self.turtle = None
+
+    def init_img_paths(self, imgs_dir: str = PLAYER_DIR):
+        self.normal = os.path.join(imgs_dir, 'normal.gif')
+        self.left1 = os.path.join(imgs_dir, 'left1.gif')
+        self.right1 = os.path.join(imgs_dir, 'right1.gif')
+        self.left2 = os.path.join(imgs_dir, 'left2.gif')
+        self.right2 = os.path.join(imgs_dir, 'right2.gif')
+
+    def init_lookups(self):
+        movt_img_pairs = (
+            (Player.Movement.LEFT.value,        self.left1),
+            (Player.Movement.LEFT.value * 2,    self.left2),
+            (Player.Movement.RIGHT.value,       self.right1),   
+            (Player.Movement.RIGHT.value * 2,   self.right2),
+            (Player.Movement.UP.value,          self.normal),
+            (Player.Movement.DOWN.value,        self.normal),
+        )
+        self.movt_lookup = dict()
+        self.img_lookup = dict()
+        for movt, img in movt_img_pairs:
+            self.movt_lookup[movt] = img
+            self.img_lookup[img] = movt
+
+    def get_movement_from(self, img_path: str):
+        return self.img_lookup.get(img_path, Player.Movement.UP.value)
+
+    def get_img_from(self, movement_path: str):
+        return self.movt_lookup.get(movement_path, self.normal)
+
+    def get_shapes(self):
+        return self.normal, self.left1, self.left2, self.right1, self.right2
+
+    def init_turtle(self):
+        self.turtle = turtle.Turtle()
+        self.turtle.shape(self.normal)
+        self.turtle.speed(0)
+        self.turtle.penup()
+        self.turtle.goto(0, -270)
+    
+    def move_left(self):
+        self.handle_shape_change(Player.Movement.LEFT)
+        x = self.turtle.xcor()
+        x = max(x - SPEED, -270)
+        self.turtle.setx(x)
+
+    def move_right(self):
+        self.handle_shape_change(Player.Movement.RIGHT)
+        x = self.turtle.xcor()
+        x = min(x + SPEED, 270)
+        self.turtle.setx(x)
+
+    def stop(self, *args, **kwargs):
+        self.handle_shape_change(Player.Movement.UP)
+
+    def get_current_shape(self):
+        return self.turtle.shape()
+
+    def handle_shape_change(self, movement: Movement):
+        current = self.get_current_shape()
+        current_movt = self.get_movement_from(current)
+        shift = movement.value
+        if movement in (Player.Movement.UP, Player.Movement.DOWN):
+            shift = -current_movt
+
+        elif movement == Player.Movement.LEFT:
+            if current == self.left1:
+                shift *= 2
+
+        elif movement == Player.Movement.RIGHT:
+            if current == self.right1:
+                shift *= 2
+
+        shift = shift + current_movt
+
+        shift = max(shift, Player.Movement.LEFT.value * 2)
+        shift = min(shift, Player.Movement.RIGHT.value * 2)
+
+        shape = self.get_img_from(shift)
+
+        self.turtle.shape(shape)
---
+++ main.py
@@ -0,0 +1,28 @@
+
+import turtle
+from Player import Player
+
+main_window = turtle.Screen()
+main_window.title("falling hearts")
+main_window.setup(width=1000, height=1000)
+
+player = Player(speed=7)
+
+for shape in player.get_shapes():
+    main_window.register_shape(shape)
+
+player.init_turtle()
+
+main_window.listen()
+
+main_window.onkeypress(player.move_left, 'Left')
+main_window.onkeypress(player.move_right, 'Right')
+main_window.onkeypress(player.stop, 'Down')
+main_window.onkeypress(player.stop, 'Up')
+canvas = main_window.getcanvas()
+
+canvas.bind("<KeyRelease-Left>", player.stop)
+canvas.bind("<KeyRelease-Right>", player.stop)
+
+
+main_window.mainloop()
