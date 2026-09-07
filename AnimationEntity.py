e45aedc45a893b3b520e19c88f30170b74ea9584
Mahmoud-Hussien-Mohamed <mahmoudhussienmohamed.mhm@gmail.com>
2026-09-07 10:14:28 +0300

    Adding AnimationEntity base class

---
+++ AnimationEntity.py
@@ -0,0 +1,101 @@
+import turtle
+from PIL import Image
+
+class AnimationEntity:
+    def __init__(
+            self, screen: turtle._Screen, img_path: str, 
+            default_x: int, default_y: int, speed: int,
+            possible_images: list[str] = None
+        ):
+        self.screen = screen
+        self.speed = speed
+        self.register_images(img_path, possible_images)
+        self.init_turtle(default_x, default_y)
+        self.update_image(img_path)
+
+    def register_images(self, default_img: str, imgs: list[str]):
+        self.screen.register_shape(default_img)
+        if not imgs: return
+        for img in imgs:
+            self.screen.register_shape(img)
+
+    def init_turtle(self, default_x: int, default_y: int):
+        self.defx = default_x
+        self.defy = default_y
+        self.turtle = turtle.Turtle()
+        self.turtle.speed(0)
+        self.turtle.penup()
+        self.goto(default_x, default_y)
+
+    def update_image(self, new_img_path: str):
+        self.img = new_img_path
+        self.width, self.height = Image.open(self.img).size
+        self.hwidth = self.width    // 2    # half width
+        self.hheight = self.height  // 2    # half height
+        self.turtle.shape(self.img)
+
+    def goto(self, x: int, y: int):
+        self.turtle.goto(x, y)
+
+    def move(self, distance_x: int, distance_y: int):
+        x = self.x + distance_x
+        y = self.y + distance_y
+        self.goto(x, y)
+
+    def move_left(self, dist: int = None):
+        dist = dist or self.speed
+        self.turtle.setx(max(self.x - dist, -500))
+
+    def move_right(self, dist: int = None):
+        dist = dist or self.speed
+        self.turtle.setx(min(self.x + dist, 500))
+
+    def move_up(self, dist: int = None):
+        dist = dist or self.speed
+        self.turtle.sety(min(self.y + dist, 500))
+
+    def move_down(self, dist: int = None):
+        dist = dist or self.speed
+        self.turtle.sety(max(self.y - dist, -500))
+
+    def return_to_default(self):
+        self.goto(self.defx, self.defy)
+
+    def is_collided_with(self, other: 'AnimationEntity'):
+        non_overlapping = (
+            self.left > other.right or  # self is completely on the right side of other
+            self.right < other.left or  # self is completely on the left side of other
+            self.top < other.bottom or  # self is completely under other
+            self.bottom > other.top     # self is completely above other
+        )
+        return not non_overlapping
+
+    @property
+    def x(self):
+        '''Object's middle point's x-coordinate'''
+        return self.turtle.xcor()
+
+    @property
+    def y(self):
+        '''Object's middle point's y-coordinate'''
+        return self.turtle.ycor()
+
+    @property
+    def left(self):
+        '''Object's least x-coordinate'''
+        return self.x - self.hwidth
+    
+    @property
+    def right(self):
+        '''Object's most x-coordinate'''
+        return self.x + self.hwidth
+
+    @property
+    def bottom(self):
+        '''Object's least y-coordinate'''
+        return self.y - self.hheight
+    
+    @property
+    def top(self):
+        '''Object's most y-coordinate'''
+        return self.y + self.hheight
