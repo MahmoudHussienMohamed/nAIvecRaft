8a88e518dadb5a03f90eee87a34d7212b99d2157
Mahmoud-Hussien-Mohamed <mahmoudhussienmohamed.mhm@gmail.com>
2026-09-10 20:30:53 +0300

    updating explosion

--- Environment/Explosions.py
+++ Environment/Explosions.py
@@ -1,5 +1,6 @@
 import os
-from Core.Environment import Environment
+import turtle
+from Core import AnimationEntity, SPEED
 
 IMGS_DIR    = os.path.abspath('./assets')
 PROJECTILES_DIR = os.path.join(IMGS_DIR, 'projectiles')
@@ -7,21 +8,18 @@ EXPLOSIONS_FRAMES_NO = 5
 EXPLOSIONS_PATHS = tuple(os.path.join(PROJECTILES_DIR, f'explosion{i}.gif') for i in range(EXPLOSIONS_FRAMES_NO))
 
 
-class Explosion(Environment):
-    def __init__(self, screen, count: int = 1):
+class Explosion(AnimationEntity):
+    def __init__(self, screen: turtle._Screen, default_x: int, default_y: int, speed: int = SPEED):
         self.fps = 10
         self.cnt = 0
         self.current = 0
-        super().__init__(screen, EXPLOSIONS_PATHS, count, speed=2)
+        super().__init__(screen, EXPLOSIONS_PATHS[0], default_x, default_y, speed, possible_images=EXPLOSIONS_PATHS)
 
-    def _on_respawn(self, item):
-        pass  # animation counter drives shape changes, not respawn
-
-    def move_down(self):
+    def move_up(self, dist: int = None):
         self.cnt += 1
-        super().move_down()
+        dist = dist or self.speed
+        self.turtle.sety(self.y + dist)
         if self.cnt >= self.fps:
-            for item in self.items:
-                self.current = (self.current + 1) % EXPLOSIONS_FRAMES_NO
-                item.shape(EXPLOSIONS_PATHS[self.current])
+            self.current = (self.current + 1) % EXPLOSIONS_FRAMES_NO
+            self.update_image(EXPLOSIONS_PATHS[self.current])
             self.cnt = 0
